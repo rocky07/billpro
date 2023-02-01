76b89706415be57dbc2a200399d404f49bfaa9df
rakeshramesan <rakesh.ramesan@tmhighland.com>
2023-01-31 21:12:26 -0600

    init commit

---
+++ src/main/java/com/ff/billpro/BillproApplication.java
@@ -0,0 +1,13 @@
+package com.ff.billpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ff/billpro/BillproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.billpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
